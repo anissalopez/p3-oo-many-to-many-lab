8430a7f496dbccfe1b0ab20eac3980298d5623ec
anissalopez <anissa965@gmail.com>
2023-11-01 14:56:27 -0400

    many to many relationship

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,85 +1,119 @@
 class Author:
+    all = []
     def __init__(self, name):
         self.name = name
+        Author.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
     
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+        else:
+            raise Exception("Name must be a string ")
+        
     def contracts(self):
         return [contract for contract in Contract.all if contract.author == self]
     
     def books(self):
-        return [contract.book for contract in Contract.all if contract.author == self]
+        return [contract.book for contract in self.contracts()]
     
     def sign_contract(self, book, date, royalties):
         return Contract(self, book, date, royalties)
     
     def total_royalties(self):
-        royalties = 0
-        for contracts in Contract.all:
-            if(contracts.author == self):
-                royalties += contracts.royalties
-
-        return royalties
+        return sum([contract.royalties for contract in self.contracts()])
+ 
     
 
 class Book:
     all = []
-
     def __init__(self, title):
-        self.title = title
-        Book.all.append(self)
+        self.title = title 
+        Book.all.append(all)
     
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._title = value
+        else:
+            raise Exception("Title must be a string")
+        
     def contracts(self):
-        return [contracts for contracts in Contract.all if contracts.book == self]
+        return [contract for contract in Contract.all if contract.book == self]
     
     def authors(self):
-        return [contracts.author for contracts in Contract.all if contracts.book == self]
-    
-   
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
     all = []
-
     def __init__(self, author, book, date, royalties):
-        if type(royalties) == int:
-            self.royalties = royalties
-        else:
-            raise Exception("royalties must be a number")
-        if isinstance (book, Book):
-            self.book = book
-        else:
-            raise Exception("book must be an instance")
-        if type(date) == str:
-            self.date = date
-        else:
-            raise Exception("date must be a string")  
-        if isinstance(author, Author):
-            self.author = author
-        else:
-            raise Exception("book must be an instance")
+        self.author = author
+        self.book = book 
+        self.date = date 
+        self.royalties = royalties 
         Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
     
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value
+        else:
+            raise Exception("Author must be an instance of the Author class")
+        
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, value):
+        if isinstance(value, Book):
+            self._book = value
+
+        else:
+            raise Exception("Book must be an instance of the Book class")
+    
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, value):
+        if isinstance(value, str):
+            self._date = value
+        else:
+            raise Exception("Date must be a string")
+        
+    @property
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, value):
+        if isinstance(value, int):
+            self._royalties = value
+        else:
+            raise Exception("Royalties must be an integer")
+        
     @classmethod
     def contracts_by_date(cls):
-        return sorted(cls.all, key=lambda x: x.date)
-
-
-    @classmethod
-    def contracts_by_dates(cls):
-        return sorted(cls.all, key=lambda x: x.date)
-    
-Contract.all = []
-author = Author("Name")
-book1 = Book("Title 1")
-book2 = Book("Title 2")
-book3 = Book("Title 3")
-contract1 = Contract(author, book1, "02/01/2001", 10)
-contract2 = Contract(author, book2, "01/01/2001", 20)
-contract3 = Contract(author, book3, "03/01/2001", 30)
-
-assert Contract.contracts_by_date() == [contract2, contract1, contract3]  
-
-print(Contract.contracts_by_date()) 
-print(Contract.contracts_by_dates()) 
+        sorted = [contract for contract in cls.all]
+        sorted.sort(key=lambda x: x.date)
+        return sorted
+        
 
 
 
